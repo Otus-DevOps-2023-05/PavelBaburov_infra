486c50fa22e294092d506bd9d318bcc2197839fb
Pavel Baburov <pashab123@yandex.ru>
2023-09-28 22:33:57 +0300

    teerraform-2 homework release v1

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "external_ip_addres_app" {
-#   value = [for ip in yandex_compute_instance.app.*.network_interface.0.nat_ip_address : ip]
-# }
-output "external_ip_address_app" {
-  value = module.app.external_ip_address_app
-}
-output "external_ip_address_db" {
-  value = module.db.external_ip_address_db
-}
