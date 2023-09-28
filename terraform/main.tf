7a484192a7f5e166a9a0551c6ba7837dcca3bfc3
Pavel Baburov <pashab123@yandex.ru>
2023-09-28 03:17:32 +0300

    working main and prod

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,16 +15,22 @@ provider "yandex" {
   zone                     = var.zone
 }
 
-module "app" {
-  source          = "./modules/app"
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
-}
-
 module "db" {
   source          = "./modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
+  connection_key_file = var.connection_key_file
+}
+
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+  connection_key_file = var.connection_key_file
+  DB_IP           = module.db.internal_ip_address_db
+  depends_on      = [
+    module.db
+  ]
 }
--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -15,18 +15,24 @@ provider "yandex" {
   zone                     = var.zone
 }
 
-module "app" {
-  source          = "../modules/app"
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
-  stage           = var.stage
-}
-
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
   stage           = var.stage
+  connection_key_file = var.connection_key_file
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+  stage           = var.stage
+  connection_key_file = var.connection_key_file
+  DB_IP           = module.db.internal_ip_address_db
+  depends_on      = [
+    module.db
+  ]
 }
